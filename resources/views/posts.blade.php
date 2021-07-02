8aea8f41b15cf568f1418e2c6b0f50f1c8ac5c99
OleksandrLevinskyi <aleksandrlevinskyi@gmail.com>
2021-07-02 15:51:41 -0400

    Set up the project

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My Blog</title>
+    <link rel="stylesheet" href="./app.css">
+</head>
+<body>
+<article>
+    <h1><a href="/posts/my-first-post">My First Post</a></h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur cupiditate dignissimos expedita fugit id,
+        labore nisi omnis repudiandae sapiente velit.</p>
+</article>
+<article>
+    <h1><a href="/posts/my-second-post">My Second Post</a></h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur cupiditate dignissimos expedita fugit id,
+        labore nisi omnis repudiandae sapiente velit.</p>
+</article>
+<article>
+    <h1><a href="/posts/my-third-post">My Third Post</a></h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur cupiditate dignissimos expedita fugit id,
+        labore nisi omnis repudiandae sapiente velit.</p>
+</article>
+</body>
+</html>
