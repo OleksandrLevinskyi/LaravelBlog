a67ca76896f96d63a6f0ece4b434571484631402
oleksandrlevinskyi <aleksandrlevinskyi@gmail.com>
2023-05-21 21:50:02 -0400

    update seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
 use App\Models\Post;
 use App\Models\User;
 use Illuminate\Database\Seeder;
@@ -15,12 +16,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $user = User::factory()->create([
-            'name'=>'Mark Tims'
-        ]);
+        User::truncate();
+        Category::truncate();
+        Post::truncate();
+
+        $user = User::factory()->create();
 
         Post::factory(5)->create([
-            'user_id'=>$user->id
+            'user_id' => $user->id
         ]);
     }
 }
